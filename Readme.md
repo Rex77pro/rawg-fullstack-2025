498c1425c79b4d57ac82553b340eaaa22ea73e87
Rex77pro <smaurieka@gmail.com>
2025-09-22 08:57:24 +0200

    feat(Update): Get newest implementation of InClass

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# Notes for Frontend-React-App
+
+## Run
+`..\rawg-fullstack-2025\rawg-frontend>` `npm run dev`
+
+## Data (games)
+- Main page: https://rawg.io/
+
+- Api Docs: https://api.rawg.io/docs/
+
--- rawg-frontend/src/components/NavBar.tsx
+++ rawg-frontend/src/components/NavBar.tsx
@@ -1,15 +1,13 @@
-import { HStack, Image, Text } from "@chakra-ui/react"
-import logo from "../assets/logo.webp"
-import { ColorModeSwitch } from "./ColorModeSwitch"
+import { HStack, Image } from "@chakra-ui/react";
 
-const NavBar = () => {
+import logo from "../assets/logo.webp";
+import { ColorModeSwitch } from "./ColorModeSwitch";
+
+export const NavBar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize={15}></Image>
-        <Text>Navbar</Text>
-        <ColorModeSwitch/>
+    <HStack justifyContent={"space-between"} padding={4}>
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
-
-export default NavBar
+  );
+};
